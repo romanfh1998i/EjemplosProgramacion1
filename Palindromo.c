6e01bb06dd6bbf0526e744665356c3d3960c0331
romanfh1998i <44349888+romanfh1998i@users.noreply.github.com>
2022-09-05 16:00:24 -0400

    Add files via upload

---
+++ Palindromo.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+void Mayuscula(char palabra[]){
+    char mayus[20];
+    for(int i=0;palabra[i]!='\0';++i){
+        mayus[i]=toupper(palabra[i]);
+    }
+    printf("la palabra en mayuscula es %s \n",mayus);
+}
+void minuscula(char palabra[]){
+    char minus[20];
+    for(int i=0;palabra[i] !='\0';++i){
+        minus[i]=tolower(palabra[i]);
+    }
+    printf("la palabra en minuscula es %s \n",minus);
+}
+
+int main (){
+    char palabra[20];    
+    int i,longitud,u=0,l=0;
+    int condicion=0;
+    printf("Digite una palabra \n");
+    scanf("%s",palabra);
+    
+    
+    longitud=strlen(palabra);
+    for(i=0;i<longitud;i++){
+        if(palabra[i]!=palabra[longitud-i-1]){
+            condicion=1;
+
+            break;
+           
+    }
+    }    
+    if(condicion){
+        printf("%s no es palindromo\n",palabra);
+        
+           
+    }else{
+        printf("%s es palindromo",palabra);
+
+    }
+    printf("la cantidad de letra en la palabra %s es =%d\n",palabra,longitud);
+    Mayuscula(palabra);
+    minuscula(palabra);
+
+}
